d681cdbb76bafdb4201bd82e98082a16c76e30e5
serotonine <ks.serotonine@gmail.com>
2025-11-06 11:10:54 +0100

    Sprint-1: Fix median.js.

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -1,14 +1,42 @@
 // Fix this implementation
-// Start by running the tests for this function
-// If you're in the Sprint-1 directory, you can run `npm test -- fix` to run the tests in the fix directory
+/*
+ * The median of an array of numbers is the middle value when all the numbers are sorted in order.
+ * Sort the numbers from smallest to largest.
+ * Then:
+ * If there’s an odd number of elements → the median is the middle number.
+ * If there’s an even number of elements → the median is the average of the two middle numbers.
+ */
 
 // Hint: Please consider scenarios when 'list' doesn't have numbers (the function is expected to return null)
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  // Check if list is an array.
+  if (!Array.isArray(list)) {
+    return null;
+  }
+  // Keep only numbers then sort ascending the list.
+  const numList = list
+    .filter((item) => typeof item === "number")
+    .sort((a, b) => a - b);
+  const lg = numList.length;
+  // Check if list.
+  if (lg === 0) {
+    return null;
+  }
+  const middleIndex = Math.floor(lg / 2);
+  // If lg is odd.
+  if (lg % 2 !== 0) {
+    const [median] = numList.splice(Math.floor(lg / 2), 1);
+    return median;
+  }
+  // If lg is even.
+  else {
+    const middleIndex = numList
+      .splice(lg / 2 - 1, 2)
+      .reduce((acc, current) => acc + current, 0);
+    return middleIndex / 2;
+  }
 }
 
 module.exports = calculateMedian;
