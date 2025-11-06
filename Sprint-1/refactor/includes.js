668ace15e236a4b2f24043b3a9a68ad139031e25
serotonine <ks.serotonine@gmail.com>
2025-11-06 17:27:55 +0100

    Sprint-1: Refactor.

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,13 +1,11 @@
 // Refactor the implementation of includes to use a for...of loop
 
 function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
-    if (element === target) {
+  for (item of list) {
+    if (item === target) {
       return true;
     }
   }
   return false;
 }
-
 module.exports = includes;
