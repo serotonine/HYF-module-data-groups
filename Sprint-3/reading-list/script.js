4aa3116f53b814f4947ba87d080abc335931d57b
serotonine <ks.serotonine@gmail.com>
2025-11-19 09:37:09 +0100

    Sprint-3: reading-list.

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -1,3 +1,32 @@
+window.addEventListener("load", ()=> {
+  const readingList = document.getElementById("reading-list");
+  for(let book of books){
+    readingList.append(createLi(book));
+
+  }
+  });
+/*
+title: 'The Most Human Human', 
+author: 'Brian Christian', 
+alreadyRead: true, 
+bookCoverImage: 'https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg'
+*/
+  function createLi({title,author,alreadyRead,bookCoverImage }){
+    const li = document.createElement("li");
+    // !alreadyRead && li.classList.add("read");
+    !alreadyRead && (li.style.backgroundColor = "red");
+    li.classList.add("book")
+    li.innerHTML = `
+        <figure class="book_image"><img src="${bookCoverImage}"></figure>
+        <div class="book_text">
+        <h3>${title}</h3>
+        <p>by ${author}</p>
+        </div>
+    `
+    return li;
+
+  }
+
 // for the tests, do not modify this array of books
 const books = [
   {
@@ -20,4 +49,5 @@ const books = [
     bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
+
 
