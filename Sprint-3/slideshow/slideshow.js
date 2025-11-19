16d6f7896706d7e883c340f4081fe1e8b83dbfbc
serotonine <ks.serotonine@gmail.com>
2025-11-19 19:10:41 +0100

    Sprint-3: Slideshow v2.

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -12,8 +12,7 @@ const step = 800;
 let currentId = 0;
 const lg = images.length - 1;
 // backward & fowards.
-let backwardActive = false;
-let fowardActive = false;
+let autoSwitchActive = false;
 let interval;
 // DOM variables.
 const slideshowImgs = document.querySelector(".slideshow_images");
@@ -23,6 +22,26 @@ const backward = document.getElementById("backward-btn");
 const autoFoward = document.getElementById("auto-forward-btn");
 const autoBackward = document.getElementById("auto-backward-btn");
 const chips = document.querySelector(".slideshow_chips");
+// Go!
+window.addEventListener("load", () => {
+  images.forEach((img, id) => {
+    slideshowImgs.append(createSlide(img, id));
+    slideshowChips.append(createChip(id));
+  });
+  foward.addEventListener("click", function () {
+    switchImg(this);
+  });
+  backward.addEventListener("click", function () {
+    switchImg(this);
+  });
+  autoFoward.addEventListener("click", function () {
+    autoSwitchImg(this);
+  });
+  autoBackward.addEventListener("click", function () {
+    autoSwitchImg(this);
+  });
+});
+
 // Populate DOM.
 function createSlide(img, id) {
   const box = document.createElement("div");
@@ -33,13 +52,6 @@ function createSlide(img, id) {
   box.append(image);
   return box;
 }
-function setActiveChip(id) {
-  for (let child of chips.children) {
-    child.dataset.id == id
-      ? child.classList.add("active")
-      : child.classList.remove("active");
-  }
-}
 function createChip(id) {
   const chip = document.createElement("div");
   chip.setAttribute("data-id", id);
@@ -49,22 +61,20 @@ function createChip(id) {
   }
   return chip;
 }
+function setActiveChip(id) {
+  for (let child of chips.children) {
+    child.dataset.id == id
+      ? child.classList.add("active")
+      : child.classList.remove("active");
+  }
+}
 // Events.
+// Used in switchImg function.
 function translateSlideShow() {
   slideshowImgs.style.transform = `translateX(${currentId * -step}px)`;
 }
-
-function goToNextImg() {
-  currentId < lg ? currentId++ : (currentId = 0);
-  setActiveChip(currentId);
-  translateSlideShow();
-}
-function goToPrevImg() {
-  currentId === 0 ? (currentId = lg) : currentId--;
-  translateSlideShow();
-}
-//
-function toggleManualNav(bool, elements) {
+// Used in autoSwitchImg function.
+function toggleDisabledBtn(bool, elements) {
   if (bool) {
     elements.forEach((el) => {
       el.classList.add("disabled");
@@ -76,38 +86,42 @@ function toggleManualNav(bool, elements) {
     });
   }
 }
-function backwardAnimation(timer = 2000) {
-  backwardActive = !backwardActive;
-  autoBackward.classList.toggle("active");
-  const elements = [backward, foward, autoFoward];
-  if (backwardActive === true) {
-    toggleManualNav(true, elements);
-    interval = setInterval(goToPrevImg, 2000);
+// Manual.
+function switchImg(el) {
+  console.log("switchImg", el);
+  switch (el.id) {
+    case "forward-btn":
+      currentId < lg ? currentId++ : (currentId = 0);
+      break;
+    case "backward-btn":
+      currentId === 0 ? (currentId = lg) : currentId--;
+      break;
+  }
+  setActiveChip(currentId);
+  translateSlideShow();
+}
+// Auto.
+function autoSwitchImg(el, timer = 2000) {
+  autoSwitchActive = !autoSwitchActive;
+  el.classList.toggle("active");
+  const elements = [backward, foward];
+  let switcher;
+  switch (el.id) {
+    case "auto-forward-btn":
+      elements.push(autoBackward);
+      switcher = foward;
+      break;
+    case "auto-backward-btn":
+      elements.push(autoFoward);
+      switcher = backward;
+      break;
+  }
+  if (autoSwitchActive === true) {
+    toggleDisabledBtn(true, elements);
+    interval = setInterval(() => switchImg(switcher), 2000);
   } else {
-    toggleManualNav(false, elements);
+    toggleDisabledBtn(false, elements);
     clearInterval(interval);
   }
 }
-function fowardAnimation() {
-  fowardActive = !fowardActive;
-  autoFoward.classList.toggle("active");
-  const elements = [backward, foward, autoBackward];
-  if (fowardActive) {
-    toggleManualNav(true, elements);
-    interval = setInterval(goToNextImg, 2000);
-  } else {
-    toggleManualNav(false, elements);
-    clearInterval(interval);
-  }
-}
-// Go!
-window.addEventListener("load", () => {
-  images.forEach((img, id) => {
-    slideshowImgs.append(createSlide(img, id));
-    slideshowChips.append(createChip(id));
-  });
-  foward.addEventListener("click", goToNextImg);
-  backward.addEventListener("click", goToPrevImg);
-  autoFoward.addEventListener("click", fowardAnimation);
-  autoBackward.addEventListener("click", backwardAnimation);
-});
+
